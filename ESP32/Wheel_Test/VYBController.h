ba13f8a68afd9e0a78fdf9babecbc15ed0f08479
SeungbinOh <osb001125@naver.com>
2024-12-26 17:20:30 +0900

    Update VYB Controller.h : add getCurrentMeasurements

--- ESP32/Wheel_Test/VYBController.h
+++ ESP32/Wheel_Test/VYBController.h
@@ -6,32 +6,41 @@
 #include "Params.h"
 #include "MGServo.h"
 
+/**
+ * @class VYBController
+ * @brief 제어 시스템의 LQR 기반 동작을 처리하는 클래스
+ */
 class VYBController {
 private:
-  MGServo& ServoRW;
-  MGServo& ServoLW;
+  MGServo& ServoRW;  ///< 우측 휠 서보
+  MGServo& ServoLW;  ///< 좌측 휠 서보
 
-  std::vector<Eigen::Matrix<float, 2, 4>> Ks;  // LQR 게인 vector
-  Eigen::Matrix<float, 2, 4> K;
-  Eigen::Matrix<float, 2, 1> u;  // input vector
+  std::vector<Eigen::Matrix<float, 2, 4>> Ks;  ///< LQR 게인 행렬들의 벡터
+  Eigen::Matrix<float, 2, 4> K;                ///< 현재 사용 중인 LQR 게인
+  Eigen::Matrix<float, 2, 1> u;                ///< 제어 입력 벡터
 
-  float iq_factor;        // (A/LSB)
-  float torque_constant;  // (Nm/A)
-  float saturation;
-  const int RW_bias = 12;
-
+  float iq_factor;         ///< 전류 변환 계수 (A/LSB)
+  float torque_constant;   ///< 토크 상수 (Nm/A)
+  float saturation;        ///< input saturation
+  const int RW_bias = 12;  ///< 우측 휠 모터의 바이어스 값
 
 public:
-  // 생성자: 시스템 파라미터 초기화
+  /**
+   * @brief 생성자: VYBController 초기화
+   * @param ServoRW_ 우측 휠 서보 객체 참조
+   * @param ServoLW_ 좌측 휠 서보 객체 참조
+   */
   VYBController(MGServo& ServoRW_, MGServo& ServoLW_)
     : ServoRW(ServoRW_), ServoLW(ServoLW_) {
+    // 전류 및 토크 상수 초기화
     iq_factor = 0.01611328f;  // (A/LSB) 33 / 2048
     torque_constant = 0.07f;  // (Nm/A)
-    // torque_constant = 0.14f;  // (Nm/A)
     saturation = iq_factor * torque_constant * MAX_TORQUE_COMMAND;
-    Eigen::Matrix<float, 2, 4> mat;
 
-    //////// Gain Hard coding /////////////////
+
+    // LQR 게인 초기화 (하드코딩된 데이터 삽입)
+    Eigen::Matrix<float, 2, 4> mat;
+    //////////////////////////////////////////////////////////
     mat << 1.08462239f, 0.12305272f, 0.19382449f, -0.19479993f,
       -1.10877978f, -0.12702366f, -0.19782372f, -0.19250106f;
     Ks.push_back(mat);
@@ -87,17 +96,38 @@ public:
     mat << 1.59324034f, 0.20509105f, 0.19530021f, -0.19342572f,
       -1.63245203f, -0.21263951f, -0.19994099f, -0.19097907f;
     Ks.push_back(mat);
+    /////////////////////////////////////////////////////////////////
   }
 
+  /**
+   * @brief 현재 제어 입력 벡터를 반환
+   * @return Eigen::Matrix<float, 2, 1> u
+   */
   Eigen::Matrix<float, 2, 1> getInputVector() {
     return u;
   }
 
+  /**
+   * @brief 모터 속도 측정을 수행하여 측정 벡터에 반영
+   * @param z 모터 속도 측정값을 저장할 벡터
+   */
   void getMotorSpeedMeasurement(Eigen::Matrix<float, 8, 1>& z) {
     z(6) = ServoRW.getMotorSpeed() * M_PI / 180;
     z(7) = ServoLW.getMotorSpeed() * M_PI / 180;
   }
 
+  /**
+  * @brief 모터 Current 측정값 update
+  * @param iq_vec 모터 current 측정값을 저장할 벡터
+  */
+  void getMotorCurrentMeasurement(Eigen::Matrix<float, 2, 1>& iq_vec){
+    iq_vec << ServoRW.getMotorIq(), ServoLW.getMotorIq();
+  }
+
+  /**
+   * @brief 현재 높이에 따라 LQR 게인 K를 계산
+   * @param h 현재 높이 (m)
+   */
   void computeGainK(const float h) {
     float temp = (h - HEIGHT_MIN) / 0.01;  // 구간을 10mm당 하나씩 나눔
     int idx = static_cast<int>(temp);      // 구간의 정수 인덱스 계산
@@ -117,33 +147,13 @@ public:
     }
   }
 
+  /**
+   * @brief 상태 벡터를 기반으로 제어 입력 벡터를 계산
+   * @param x_d 목표 상태 벡터
+   * @param x 현재 상태 벡터
+   */
   void computeInput(Eigen::Matrix<float, 4, 1>& x_d, Eigen::Matrix<float, 4, 1>& x) {
     u = K * (x_d - x) / 2;
-
-    // Serial.print("x:");
-    // Serial.print(x(0));
-    // Serial.print(x(1));
-    // Serial.print(x(2));
-    // Serial.println(x(3));
-
-    // Serial.print("x_d:");
-    // Serial.print(x_d(0));
-    // Serial.print(x_d(1));
-    // Serial.print(x_d(2));
-    // Serial.println(x_d(3));
-
-    // Serial.println("K:");
-    // for (int i = 0; i < K.rows(); ++i) {
-    //   for (int j = 0; j < K.cols(); ++j) {
-    //     Serial.print(K(i, j), 6);  // 소수점 6자리까지 출력
-    //     Serial.print(" ");
-    //   }
-    //   Serial.println();
-    // }
-    // Serial.print("u:");
-    // Serial.print(u(0));
-    // Serial.print(u(1));
-
 
     // Input saturation
     for (int j = 0; j < 2; j++) {
@@ -153,31 +163,36 @@ public:
         u(j) = -saturation;
       }
     }
-    // Serial.print("Saturation : ");
-    // Serial.print(saturation, 10);
-    // Serial.print("u(after):");
-    // Serial.print(u(0),10);
-    // Serial.println(u(1),10);
-
-    // Serial.print(" Constant:");
-    // Serial.print(MAX_TORQUE_COMMAND, 10);
-    // Serial.print(" ");
-    // Serial.print(iq_factor,10);
-    // Serial.print(" ");
-    // Serial.println(torque_constant,10);
   }
 
+  /**
+   * @brief 계산된 제어 명령을 서보에 전송
+   */
   void sendControlCommand() {
     float u_RW = u(0) / (iq_factor * torque_constant);
     float u_LW = u(1) / (iq_factor * torque_constant);
 
+    // Right Wheel motor의 마찰로 인해 발생하는 torque 문제를 조정해줌
     if (u_RW < 0) {
       u_RW -= RW_bias;
     } else if (u_RW > 0) {
       u_RW += RW_bias;
     }
 
+    ServoRW.sendTorqueControlCommand(static_cast<int16_t>(u_RW));
+    ServoLW.sendTorqueControlCommand(static_cast<int16_t>(u_LW));
+  }
 
+  void sendDirectControlCommand(Eigen::Matrix<float, 2, 1> u_) {
+    float u_RW = u_(0) / (iq_factor * torque_constant);
+    float u_LW = u_(1) / (iq_factor * torque_constant);
+
+    // Right Wheel motor의 마찰로 인해 발생하는 torque 문제를 조정해줌
+    if (u_RW < 0) {
+      u_RW -= RW_bias;
+    } else if (u_RW > 0) {
+      u_RW += RW_bias;
+    }
 
     ServoRW.sendTorqueControlCommand(static_cast<int16_t>(u_RW));
     ServoLW.sendTorqueControlCommand(static_cast<int16_t>(u_LW));
