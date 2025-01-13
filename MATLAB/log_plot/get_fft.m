400b6939348c1a95fbd9ebc03818c5a7794dc904
SeungbinOh <osb001125@naver.com>
2025-01-13 20:37:24 +0900

    choir: Update Convinient plotter in data_plotter and Read_VICON_data

--- MATLAB/log_plot/get_fft.m
+++ MATLAB/log_plot/get_fft.m
@@ -1,4 +1,17 @@
 function [f, P1] = get_fft(raw_data, Ts)
+    % get_fft computes the single-sided amplitude spectrum of a signal using FFT.
+    % 
+    % This function takes in a time-domain signal (`raw_data`) and its sampling time interval (`Ts`),
+    % performs a Fast Fourier Transform (FFT), and returns the corresponding frequency vector (`f`)
+    % and the single-sided amplitude spectrum (`P1`).
+    %
+    % Inputs:
+    %   raw_data - Time-domain signal (1D array)
+    %   Ts - Sampling time interval (scalar)
+    %
+    % Outputs:
+    %   f - Frequency vector (1D array)
+    %   P1 - Single-sided amplitude spectrum (1D array)
     L = length(raw_data);
     sampling_rate = 1/Ts;
     Raw_FFT = fft(raw_data);
